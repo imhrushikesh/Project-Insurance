20b6590a9dcf229a45a5e25b3b3fe54d001c94b6
imhrushikesh <hrushikeshdurgude76497@gmail.com>
2023-02-12 15:45:27 +0530

    folder structure and data dump in database compledted

---
+++ data_dump.py
@@ -0,0 +1,21 @@
+import pymongo 
+import pandas as pd
+import json
+
+client = pymongo.MongoClient("mongodb+srv://Hrushikesh:rishi0707@cluster0.hadmu.mongodb.net/?retryWrites=true&w=majority")
+
+DATA_FILE_PATH = ("/config/workspace/insurance.csv")
+DATABASE_NAME = "INSURANCE"
+COLLECTION_NAME = "INSURANCE_PROJECT"
+
+
+if __name__=="__main__":
+    df = pd.read_csv(DATA_FILE_PATH)
+    print(f"Rows and columns: {df.shape}")
+
+    df.reset_index(drop = True, inplace = True)
+
+    json_record = list(json.loads(df.T.to_json()).values())
+    print(json_record[0])
+
+    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
