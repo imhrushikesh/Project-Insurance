bb1fb9b2fc87bc9305514527212358a292e181b1
imhrushikesh <hrushikeshdurgude76497@gmail.com>
2023-02-13 18:16:37 +0530

    model evaluation & model pusher

--- Insurance/components/model_evaluation.py
+++ Insurance/components/model_evaluation.py
@@ -9,6 +9,7 @@ from Insurance import utils
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 from Insurance.predictor import ModelResolver
+from Insurance.config import TARGET_COLUMN
 
 
 
@@ -43,6 +44,66 @@ class ModelEvaluation:
                 return model_eval_artifact
 
 
-            
+            # Find location previous model
+            transformer_path = self.model_resolver.get_latest_transformer_path()
+            model_path = self.model_resolver.get_latest_model_path()
+            traget_encoder_path = self.model_resolver.get_latest_target_encoder_path()
+
+
+            # Previosu model
+            transformer = load_object(file_path=transformer_path)
+            model = load_object(file_path=model_path)
+            target_encoder = load_object(file_path=target_encoder_path)
+
+
+            # New  model
+            current_transformer = load_object(file_path=self.data_transformation_artifact.transform_object_path)
+            current_model = load_object(file_path=self.model_trainer_artifact.model_path)
+            current_target_encoder = load_object(file_path=self.data_transformation_artifact.target_encoder_path)
+
+            test_df = pd.read_csv(self.data_ingestion_artifact.test_file_path)
+            target_df = test_df[TARGET_COLUMN]
+            y_ture = target_df
+
+            input_features_name = list(transformer.feature_names_in_)
+            for i in input_features_name:
+                if test_df[i].dtypes == 'object':
+                    test_df[i] = target_encoder.fit_transform(test_df[i])
+
+            input_arr = transformer.transform(test_df[input_features_name])
+            y_pred = model.predict(input_arr)
+
+
+            # Comparision b/w new model and old model
+
+            previous_model_score = r2_score(y_true=y_ture, y_pred=y_pred)
+
+
+            # Accuracy current model
+
+            input_feature_name = list(current_transformer.feature_names_in_)
+            input_arr = current_transformer.transform(test_df[input_feature_name])
+            y_pred = current_model.predict(input_arr)
+            y_true = target_df
+
+
+            current_model_score = r2_score(y_true=y_true, y_pred=y_pred)
+
+
+            # FInal comparision between both model
+            if current_model_score <= previous_model_score:
+                logging.info(f"Current trained model is not better than previous model")
+                raise Exception("Current model is not better than previous model")
+
+
+            model_eval_artifact = artifact_entity.ModelEvaluationArtifact(is_model_accepted =  True, 
+            improved_accuracy=current_model_score - previous_model_score)
+
+            return model_eval_artifact
+
         except Exception as e:
             raise InsuranceException(e, sys)
+
+
+# Cloud ( AWS -> s3 bucket)
+# Databse - > Model pusher
